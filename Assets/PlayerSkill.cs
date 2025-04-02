d88756afb502427bcee58a93e4e244319dc56957
QuangVinh <lequangvinh2001@gmail.com>
2025-04-02 13:36:35 +0700

    New

--- Assets/PlayerSkill.cs
+++ Assets/PlayerSkill.cs
@@ -2,27 +2,48 @@
 
 public class PlayerSkill : MonoBehaviour
 {
-    public GameObject blastWavePrefab; 
+    public GameObject skillImage;  // Tham chiếu tới đối tượng Skill Image chứa Animator
+    public GameObject blastWavePrefab;  // Prefab của kỹ năng
+    private Animator skillAnimator;  // Animator của Skill Image
     private float delayBeforeNextCast = 1f;
     private bool nextCastTime = true;
+
+    private void Awake()
+    {
+        skillAnimator = skillImage.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        
+
+    }
+
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q) && nextCastTime)
         {
-            CastBlastWave(); 
+            ActivateSkill();
         }
     }
 
-    void CastBlastWave()
+    void ActivateSkill()
     {
-        nextCastTime = false; 
-        Invoke(nameof(EnableSkill), delayBeforeNextCast); 
+        nextCastTime = false;
+
+        if (skillAnimator != null)
+        {
+            skillAnimator.SetTrigger("PlayPopup");
+        }
+
         Instantiate(blastWavePrefab, transform.position, Quaternion.identity);
+
+        Invoke(nameof(EnableSkill), delayBeforeNextCast);
     }
 
     void EnableSkill()
     {
-        nextCastTime = true; 
+        nextCastTime = true;
     }
 }
