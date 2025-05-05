bfaf94cbccdbfe86b9dd38b69268638f84753ba9
vinhquangg <85091906+vinhquangg@users.noreply.github.com>
2025-05-05 19:47:58 +0700

    New

---
+++ Assets/Script/Sound/SoundEffectLibrary.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectLibrary : MonoBehaviour
+{
+    [SerializeField] private SoundEffectGroup[] soundEffectGroups;
+    private Dictionary<string, List<AudioClip>> soundEffectDictionary;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void InitializeDictionary()
+    {
+        soundEffectDictionary = new Dictionary<string, List<AudioClip>>();
+
+        foreach(SoundEffectGroup soundEffectGroup in soundEffectGroups)
+        {
+            soundEffectDictionary[soundEffectGroup.name] = soundEffectGroup.soundEffects;
+        }
+    }
+
+    public AudioClip GetRandomClip(string name)
+    {
+        if (soundEffectDictionary.ContainsKey(name))
+        {
+            List<AudioClip> audioClips = soundEffectDictionary[name];
+            if (audioClips.Count > 0)
+            {
+                return audioClips[UnityEngine.Random.Range(0, audioClips.Count)];
+            }
+        }
+        return null;
+    }
+
+    [System.Serializable]
+    public struct SoundEffectGroup
+    {
+        public string name;
+        public List<AudioClip> soundEffects;
+    }
+}
---
+++ Assets/Script/Sound/SoundManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    private static AudioSource audioSource;
+    private SoundEffectLibrary soundEffectLibrary;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            audioSource = GetComponent<AudioSource>();
+            soundEffectLibrary = GetComponent<SoundEffectLibrary>();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public static void Play(string soundName)
+    {
+        AudioClip audioClip = instance.soundEffectLibrary.GetRandomClip(soundName);
+        if(audioClip != null)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+
+    private void Start()
+    {
+        sfxSlider.onValueChanged.AddListener(delegate { OnValueChanged(); });
+    }
+
+    public static void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    public void OnValueChanged()
+    {
+        SetVolume(sfxSlider.value);
+    }
+}
